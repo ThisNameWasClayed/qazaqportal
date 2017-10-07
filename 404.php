ff4fc126d0948a868615ae676d45a768d7b874b0
ThisNameWasClayed <32436615+ThisNameWasClayed@users.noreply.github.com>
2017-10-08 01:12:45 +0600

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Qazaq Portal</title>
+
+	<!-- Logo -->
+
+	<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico"/>
+
+	<!-- CSS -->
+
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+
+	<!-- Font -->
+
+	<link rel="stylesheet" type="text/css" href="../fonts/fonts.css">
+
+	<!-- Font Awesome -->
+
+	<link rel="stylesheet" type="text/css" href="../css/font-awesome.css">
+
+	<!-- jQuery -->
+
+	<script type="text/javascript" src="../js/jquery-3.2.1.min.js"></script>
+
+	<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+
+	<!-- Bootstrap -->
+
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+		<!-- JS -->
+		<script type="text/javascript" src="../js/bootstrap.js"></script>
+
+</head>
+<body>
+	<div id="f404">
+
+		<div class="header404">
+			<a href="/qazaqportal" class="flogo">Q <span class="fline">|</span> Portal</a>
+		</div>
+
+		<div class="f404-info">
+			<p class="fmain-text">ЭТО ФИАСКО,<br>БРАТАН!</p>
+			<div class="f404-img">
+				<img src="../img/404Tl.png">
+			</div>
+		</div>
+
+		<div class="footer404">Made with <i class="fa fa-heart"></i> by Alihan</div>
+
+		<script>
+
+			$(document).ready(function(){
+				$(".footer404").css('height', '25px');
+			});
+
+		</script>
+
+	</div>
+</body>
+</html>
